eea18f55b77ee3c33f8c70801e48ae05186c947e
Kailash Khadka <k.khadka343@gmail.com>
2024-12-18 22:32:29 +0000

    Cartographer_slam

---
+++ cartographer_slam/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'cartographer_slam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/cartographer_slam/launch', glob('launch/*.launch.*')),
+        ('share/cartographer_slam/config', glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='k.khadka343@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
